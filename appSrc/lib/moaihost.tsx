0e2762e497b6e1d91541de55b923521d4fd20e51
halfnelson <dpershouse@gmail.com>
2016-08-06 13:03:22 +1000

    reformat and ensure basic run works

--- appSrc/lib/moaihost.tsx
+++ appSrc/lib/moaihost.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-declare var MoaiPlayer:any;
+declare var MoaiPlayer: any;
 
 const path = require('path');
 
@@ -35,16 +35,16 @@ export class MoaiHost extends React.Component<IHostProps, IHostState> {
 
     componentDidMount() {
         console.log("creating new player");
-        
-        var appDir = path.join(window['appDir'],"lua");
+
+        var appDir = path.join(window['appDir'], "lua");
 
         var player = new MoaiPlayer($("#moaiplayer"));
-        
+
         player.hideInfo();
         if (!player.moai) { player.initMoai(); }
         player.moai.getEmscripten();
         player.moai.emscripten.FS_mkdir('/editor');
-        var editor_mount = player.moai.emscripten.FS_mount(player.moai.emscripten.FS_filesystems.NODEFS, { root: appDir+"\\" }, '/editor');
+        var editor_mount = player.moai.emscripten.FS_mount(player.moai.emscripten.FS_filesystems.NODEFS, { root: appDir + "\\" }, '/editor');
 
         player.moai.emscripten.FS_mkdir('/project');
         var mount = player.moai.emscripten.FS_mount(player.moai.emscripten.FS_filesystems.NODEFS, { root: this.props.sourcePath }, '/project');
@@ -53,33 +53,28 @@ export class MoaiHost extends React.Component<IHostProps, IHostState> {
             player: player,
             mount: mount,
             editor_mount: editor_mount
-        }) 
+        })
 
         //needs to be done on next turn after emscripten is initialized
-        window.setTimeout(function() { 
-              console.log("MoaiJS Filesystem Loaded");	
-  	          player.moai.emscripten.run();   
-		      player.moai.restoreDocumentDirectory();
+        window.setTimeout(function () {
+            console.log("MoaiJS Filesystem Loaded");
+            player.moai.emscripten.run();
+            player.moai.restoreDocumentDirectory();
 
-                player.moai.hostinit();
-                player.moai.AKUSetWorkingDirectory('/editor');
-	            player.moai.AKURunScript("main.lua");
-                
-                //player.moai.pause();
-                //player.moai.updateloop();
-        }.bind(this),0);
-        
-        
+            player.moai.hostinit();
+            player.moai.AKUSetWorkingDirectory('/editor');
+            player.moai.AKURunScript("main.lua");
+
+            //player.moai.pause();
+            //player.moai.updateloop();
+        }.bind(this), 0);
+
+
     }
-   
+
     render() {
-        return ( 
+        return (
             <div id="moaiplayer" className="moai-player" data-url="" data-title="My Moai App" data-ram="64">
-                <div id="runMe" onClick={this.dorun}><i className="fa fa-play"></i></div>
-                <div className="moai-cpal">
-                    <img src="lib/moai-host/moailogo-white.svg"/>
-                    <a href="http://www.getmoai.com">www.getmoai.com</a>
-                </div>
             </div>
         );
     }
--- appSrc/main.tsx
+++ appSrc/main.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import { MoaiHost } from './lib/moaihost';
-const { ipcRenderer } = require('electron'); 
+const { ipcRenderer } = require('electron');
 const { dialog } = require('electron').remote;
 
-declare var window:Window;
+declare var window: Window;
 
 var srcPath = "k:\\dev\\moai-projects\\test175\\src\\";
 
-ipcRenderer.on('open-project', function() {
-    var selected = dialog.showOpenDialog({properties: ['openDirectory']});
+ipcRenderer.on('open-project', function () {
+    var selected = dialog.showOpenDialog({ properties: ['openDirectory'] });
     if (selected && selected.length > 0) {
         srcPath = selected[0];
         ReactDOM.render(<MoaiHost sourcePath={srcPath} key={srcPath} />, document.getElementById('root'));
-    }    
+    }
 });
 
 
