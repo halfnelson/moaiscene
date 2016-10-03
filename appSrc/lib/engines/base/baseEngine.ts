7d56798bf75d31643ed96635ec7452f5dd42841f
halfnelson <dpershouse@gmail.com>
2016-10-03 14:42:34 +1000

    mock out a basic json tree scenegraph engine

--- appSrc/lib/engines/base/baseEngine.ts
+++ appSrc/lib/engines/base/baseEngine.ts
@@ -1,20 +1,81 @@
 import { Scene } from '../../scene';
 import { SceneEngine, SceneEngines } from '../../sceneEngines'
 import { SceneCommand, ConstructCommand, DeleteCommand, PropertySetCommand } from '../../sceneCommands'
-import { SceneTree } from '../../sceneObject'
+import { SceneObject, SceneTree } from '../../sceneObject'
 
 class BaseEngine implements SceneEngine {
     name: string = "base";
-    async executePropertySetCommand(command: PropertySetCommand, sceneTree: SceneTree): Promise<void> {
-        command.object.properties[command.propertyName] = command.newValue;
+    sg: any;
+
+    private getObject(o: SceneObject): any {
+        if (o.parent) {
+            return this.getObject(o.parent)[o.name];
+        } else {
+            return this.sg[o.name];
+        }
     }
+
+    async executePropertySetCommand(command: PropertySetCommand, sceneTree: SceneTree): Promise<void> {
+      
+        var obj = this.getObject(command.object);
+
+        //special change parent
+        if (command.propertyName == "parent" && command.newValue.kind == "ref") {
+            var newParent = command.newValue.value ? this.getObject(command.newValue.value): null;
+            var oldParent = command.object.parent ? this.getObject(command.object.parent) : null;
+            
+            if (newParent) {
+                newParent[command.object.name] = obj;
+                command.object.parent = command.newValue.value;
+            } else {
+                this.sg[command.object.name] = obj; 
+                command.object.parent = null;
+            }
+            if (oldParent) {
+                delete oldParent[command.object.name];
+            } else {
+                delete this.sg[command.object.name];
+            }
+
+            return Promise.resolve();
+        }
+
+        //change name
+        if (command.propertyName == "name" && command.newValue.kind == "scalar") {
+            var parent = command.object.parent ? this.getObject(command.object.parent) : this.sg;
+            parent[command.newValue.value] = obj;
+            if (command.newValue.value != command.object.name) {
+                delete parent[command.object.name];
+            }
+            command.object.name = command.newValue.value;
+            return Promise.resolve();
+        }
+        
+        //set prop
+        command.object.properties[command.propertyName] = command.newValue;
+
+        if (command.newValue.kind == "scalar") {
+           obj[command.propertyName] = command.newValue.value;
+        } else {
+           obj[command.propertyName] = this.getObject(command.newValue.value)
+        }
+    }
+
+
     async executeConstructCommand(command: ConstructCommand, sceneTree: SceneTree): Promise<void> {
         sceneTree.append(command.object);
+        var parent = command.object.parent ? this.getObject(command.object.parent) : this.sg;
+        parent[command.object.name] = { type: command.object.type }
     }
+
     async executeDeleteCommand(command: DeleteCommand, sceneTree: SceneTree): Promise<void> {
         sceneTree.remove(command.object);
+        var parent = command.object.parent ? this.getObject(command.object.parent) : this.sg;
+        delete parent[command.object.name]; 
     }
+    
     async newScene(): Promise<void> {
+        this.sg = {};
         return Promise.resolve();
     }
 } 
